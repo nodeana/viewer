684319c055806fe97eade83ca34625a0649c0b7c
Nodeana <33127573+nodeana@users.noreply.github.com>
2024-10-05 09:41:23 +0200

    cloudflare to get ipv4

--- ip.js
+++ ip.js
@@ -1,6 +1,7 @@
 export async function getClientIP() {
-  const response = await fetch("https://ip.viewer.sh");
+  const response = await fetch("https://1.0.0.1");
   const data = await response.text();
+  const ipMatch = data.match(/ip=([0-9.]+)/);
 
-  return data;
+  return ipMatch[1];
 }
